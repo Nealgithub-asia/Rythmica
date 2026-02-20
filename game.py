f07fdb1a22db937bedfff3ada684345eb0c58cbe
neal <singhneal29@gmail.com>
2026-02-20 19:27:30 +0530

    randomized directions

--- game.py
+++ game.py
@@ -7,19 +7,28 @@ y,sr=librosa.load(path)
 
 beat_track=librosa.onset.onset_detect(y=y,sr=sr)
 beat_times=librosa.frames_to_samples(beat_track)
-total_beats=len(beat_times)
+total_beats=6
 #print(f"beat times:\n{beat_times/sr}")
-print(len(beat_times)) #total beats
 
-direction=np.array(["Top","Bottom","Left","Right"])
-
+#randomizes the top,bottom,left and right
+direction=["Top","Bottom","Left","Right"]
 arx=[]
 for i in range(total_beats):
     num= np.random.randint(1,4)
-    randomize=np.random.randint(0,5,size=(num)).tolist()
+    randomize=np.random.randint(0,4,size=(num)).tolist()                           
     arx.append(randomize)
-    #print(arx)
+arxd=[]
+for i in range(len(arx)):
+    for j in range(len(arx[i])):
+        match j:
+            case 0:
+                arxd[i][j]=direction[0]
+                
+            case 1:
+                arxd[i][j]=direction[1]
+            case 2:
+                arxd[i][j]=direction[2]
+            case 3:
+                arxd[i][j]=direction[3]
+
 print(arx)
-
-
-
