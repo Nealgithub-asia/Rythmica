8b2da29a73e5668d6b0459a82de477c31717fdd2
neal <singhneal29@gmail.com>
2026-02-23 19:56:30 +0530

    created the foundation for amplitude(RMS) based direction count

--- game.py
+++ game.py
@@ -1,12 +1,15 @@
 import librosa
 import numpy as np
 import copy
+import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=2, suppress=True)
 
 path = "./music/tunetank-vlog-beat-background-349853.mp3"
-y,sr=librosa.load(path)
+example = librosa.example("nutcracker")
 
+y,sr=librosa.load(path)
+#22050 = sr
 beatTrack=librosa.onset.onset_detect(y=y,sr=sr)
 beatSamples=librosa.frames_to_samples(beatTrack)
 beatTime=beatSamples/sr
@@ -14,11 +17,29 @@ beatTime=beatSamples/sr
 totalBeats=len(beatTime)
 #print(f"beat times:\n{beatTime/sr}")
 
+z=np.empty(totalBeats)
+for a,i in enumerate(beatSamples):
+    z[a]=np.abs(y[i])
+x=y.tolist()
+#calculating RMS
+n=np.sort(z)
+print(f"median:{np.median(n) :.3f}")
+RMS=np.sqrt(np.mean(np.square(z)))
+print(f"mean: {z[int(len(z)/2)] :.3f},max :{max(np.abs(z)) :.3f},min :{min(np.abs(z)) :.3f}")
+print(f"RMS: {RMS :.3f}")
+
+
 #randomizes the top,bottom,left and right
 direction=["Top","Bottom","Left","Right"]
 randirectionIndex=[]
-for i in range(totalBeats):
-    num= np.random.randint(1,4)
+for i in beatSamples:
+    if(np.abs(y[i])>RMS):
+
+        num= np.random.randint(3,4)
+    elif(np.abs(y[i])<=RMS):
+
+        num= np.random.randint(1,3)
+
     randomize=np.random.choice(4,size=(num), replace = False).tolist()                           
     randirectionIndex.append(randomize)
 
@@ -41,7 +62,14 @@ beatDirection=beatTime.tolist()
 for i in range(totalBeats):
     beatDirection[i]=[beatDirection[i], randirection[i]]
 
-print(randirectionIndex[:3])
-print(randirection[:3])
-print(beatDirection[:3])
 
+
+
+print(z[:5],beatDirection[:5],sep="\n")
+#show music graph
+"""
+plt.figure(figsize=(10, 4))
+librosa.display.waveshow(y, sr=sr)
+plt.title('Waveform')
+plt.show()
+"""
